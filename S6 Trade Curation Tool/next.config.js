b906ab83173fa79d6cae622cc80cd051a62708f3
John Alderman <jalderman@gmail.com>
2026-05-28 13:32:16 -0400

    add webpack alias for @ imports

--- S6 Trade Curation Tool/next.config.js
+++ S6 Trade Curation Tool/next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -9,6 +11,10 @@ const nextConfig = {
   },
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
 };
 
 module.exports = nextConfig;
